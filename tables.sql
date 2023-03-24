fccba15be8ec7fc45604a718f4f8557323c38022
Matheus <matheuscerutti96@gmail.com>
2023-03-24 16:16:31 -0300

    Ajuste Final

--- tables.sql
+++ tables.sql
@@ -2,25 +2,25 @@ CREATE DATABASE "laboleria";
 
 CREATE TABLE "cakes" (
 	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR,
-	"price" NUMERIC,
-	"image" VARCHAR,
+	"name" VARCHAR UNIQUE NOT NULL,
+	"price" NUMERIC NOT NULL,
+	"image" VARCHAR NOT NULL,
 	"description" text
 );
 
 CREATE TABLE "clients" (
 	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR,
-	"address" VARCHAR,
-	"phone" VARCHAR
+	"name" VARCHAR UNIQUE NOT NULL,
+	"address" VARCHAR NOT NULL,
+	"phone" VARCHAR NOT NULL
 );
 
 
 CREATE TABLE "orders" (
 	"id" SERIAL PRIMARY KEY,
-	"clientId" INTEGER,
-	"cakeId" INTEGER,
-	"quantity" INTEGER,
+	"clientId" INTEGER NOT NULL REFERENCES "clients"("id"),
+	"cakeId" INTEGER NOT NULL REFERENCES "cakes"("id"),
+	"quantity" INTEGER NOT NULL,
 	"createdAt" TIMESTAMP DEFAULT NOW() NOT NULL,
-	"totalPrice" NUMERIC
+	"totalPrice" NUMERIC NOT NULL
 );
